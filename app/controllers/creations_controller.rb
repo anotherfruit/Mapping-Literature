4ed053894c845cdb45ace141ad7cbc9da31d1d1e
Bryan Larsen <bryan@larsen.st>
2012-08-13 15:59:22 -0400

    fixup the new fragment / new anchor links so that they work from the new creation / new fragment pages.

--- app/controllers/creations_controller.rb
+++ app/controllers/creations_controller.rb
@@ -4,4 +4,15 @@ class CreationsController < ApplicationController
 
   auto_actions :all
   autocomplete
+
+  def create
+    hobo_create do
+      if valid?
+        if params["after_submit"].include?("creations//fragment")
+          params["after_submit"].gsub!("creations//fragments", "creations/#{this.id}/fragments")
+        end
+      end
+      create_response(:new)
+    end
+  end
 end
--- app/controllers/fragments_controller.rb
+++ app/controllers/fragments_controller.rb
@@ -2,12 +2,18 @@ class FragmentsController < ApplicationController
 
   hobo_model_controller
 
-  auto_actions :all
+  auto_actions :all, :except => [:new, :create]
   index_action :rss
   auto_actions_for :creation, [ :index, :new, :create ]
 
-  def view
-
+  def create_for_creation
+    hobo_create do
+      if valid?
+        if params["after_submit"] =~ /(fragment=)0($|&)/
+          params["after_submit"].gsub!(/(fragment=)0($|&)/, "\\1#{this.id}\\2")
+        end
+      end
+      create_response(:new_for_creation)
+    end
   end
-
 end
